9606faf6ab5df0d63a5c8f0d1c1ff3c672c8a15a
jotaMontano <jmoyam@ucenfotec.ac.cr>
2017-04-27 23:39:49 -0600

    Ajedrez,Damas y go cambio de extends to implements TipoJuego

--- src/Juegos/Damas.java
+++ src/Juegos/Damas.java
@@ -1,5 +1,12 @@
 package Juegos;
 
-public class Damas extends TipoJuego{
+import java.util.ArrayList;
+
+public class Damas implements TipoJuego{
+	@Override
+	public void jugar(ArrayList piezas) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
