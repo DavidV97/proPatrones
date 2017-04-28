5c7b20c96f986d78471a826def4fdd72563e4056
jotaMontano <jmoyam@ucenfotec.ac.cr>
2017-04-27 23:47:52 -0600

    Piezas metodos color y coordenadas

--- src/PiezasDeJuego/Piezas.java
+++ src/PiezasDeJuego/Piezas.java
@@ -1,5 +1,10 @@
 package PiezasDeJuego;
 
 public interface Piezas {
-
+	public String color  = "";
+	public String coordenadas = "";
+	
+	public void color(String pcolor);
+	
+	public void coordenadas(String pcoordenadas);
 }
