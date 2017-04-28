a2dc2ddd4a981fede63e377166221690f1070382
jotaMontano <jmoyam@ucenfotec.ac.cr>
2017-04-28 00:36:37 -0600

    metodo crearPieza

--- src/PiezasDeJuego/Piezas.java
+++ src/PiezasDeJuego/Piezas.java
@@ -1,10 +1,14 @@
 package PiezasDeJuego;
 
+import java.util.ArrayList;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 public interface Piezas {
 	public String color  = "";
 	public String coordenadas = "";
 	
+	public ArrayList crearPiezas();
 	public void color(String pcolor);
-	
 	public void coordenadas(String pcoordenadas);
 }
