b66ad1a3181d75d4f63924633c18193409081bf7
jotaMontano <jmoyam@ucenfotec.ac.cr>
2017-05-05 15:48:37 -0600

    Conflicto resuelto

--- src/Main.java
+++ src/Main.java
@@ -31,11 +31,14 @@ public class Main {
 	static void showMenu() {
 		out.println();
 
-		out.println("    -- MEN� PRINCIPAL --    ");
-		out.println("1.Crear jugador ");
-		out.println("2.Escoger tipo de juego  ");
-		out.println("3. imprimirTablero ");
-		out.println("4. mover Piezas ");
+		out.println("    -- MENÚ PRINCIPAL --    ");
+		out.println("1. Crear jugador ");
+		out.println("2. Escoger tipo de juego  ");
+		out.println("3. ImprimirTablero ");
+		out.println("4. Mover Piezas ");
+		out.println("5. Seleccionar jugadores para el juego");
+		out.println("0. Salir ");
+
 	}
 
 
@@ -48,28 +51,33 @@ public class Main {
 
 			showMenu();
 			option = readInput();
+			
+			switch(option){			
 
-			if (option.equals("1")) {
+			case "1":
 				createJugador();
-			} else if (option.equals("2")) {
+				break;
+			case "2":
 				out.println("1- Ajedrez\n 2- Damas\n 3- Go  ");
 				String tipoJuego = readInput();
 				escogerJuego(tipoJuego);
-
-			} else if (option.equals("3")) {
+				break;
+			case "3":
 				imprimirTablero();
-
-			} else if (option.equals("4")) {
-				moverPiezas();
-
-
-			} else if (option.equals("0")) {
+				break;
+			case "4":
+				Jugar();
+				break;
+			case "5":
+				selecJugadores();
+				break;
+			case "0":
 				exit = true;
 				out.println("-- Hasta pronto --");
-
-			} else {
+				break;
+			default:
 				out.println("-- Opcion invalida --");
-				out.println();
+				break;
 			}
 		}
 	}
@@ -82,54 +90,92 @@ public class Main {
 		pPassword = getPassword();
 
 		gestor.createJugador(pUsername, pEmail, pPassword);
+		out.println("Jugador creado de manera exitosa");
 	}
 
 	static String getUsername() throws IOException {
-		String username = "";
-		boolean reprobate = true;
+		String username = ""; 
+		
+		out.println("Nombre de usuario: ");
+		username = readInput();
 
-		while (reprobate) {
-			out.println("Nombre de usuario: ");
-			username = readInput();
-			if (username != null && username != "" && gestor.checkExists(username)) {
-				reprobate = false;
-			}
+		if(gestor.checkExists(username)){
+			out.println("El usuario digitado ya existe.");
+			getUsername();
 		}
 		return username;
 	}
 
 	static String getEmail() throws IOException {
 		String email = "";
-		boolean reprobate = true;
 		Pattern pattern = Pattern.compile(
 				"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 		Matcher mather;
 
-		while (reprobate) {
-			out.println("E-mail: ");
-			email = readInput();
-			mather = pattern.matcher(email);
-			if (email != null && email != "" && mather.find()) {
-				reprobate = false;
-			}
+		out.println("E-mail: ");
+		email = readInput();
+		mather = pattern.matcher(email);
+		
+		if(!mather.find()){
+			out.println("El formato del correo digitado es ivalido.");
+			getEmail();
 		}
 		return email;
 	}
 
 	static String getPassword() throws IOException {
 		String password = "";
-		boolean reprobate = true;
 
-		while (reprobate) {
-			out.println("Contrase�a: ");
-			password = readInput();
-			if (password != null && password != "") {
-				reprobate = false;
+		out.println("Contraseña: ");
+		password = readInput();
+
+			if(password.length() < 6){
+				out.println("La contraseña debe tener minimo 6 caracteres.");
+				getPassword();
 			}
-		}
 		return password;
 	}
+	
+	static void selecJugadores() throws IOException{
+		String username1,username2;
+		
+		if(gestor.getQuanJug() > 0){
+			out.println(gestor.getDBJugadores());
+			out.println("Digite el nombre de usuario del jugador 1");
+			username1 = readInput();
+			out.println("Digite el nombre de usuario del jugador 2");
+			username2 = readInput();
+			if(gestor.checkExists(username1)){
+				if(gestor.checkExists(username2)){
+					gestor.setJugador(username1,username2);
+					out.println("Jugadores seleccionados de manera exitosa.");
+				}else{
+					out.println("El nombre del jugador 2 no existe, intente otra vez.");
+					selecJugadores();
+				}
+			}else{
+				out.println("El nombre del jugador 1 no existe, intente otra vez.");
+				selecJugadores();
+			}
+		}else{
+			out.println("Primero debe de crear jugadores.");
+		}
+	}
 
+	public static boolean validateGame() throws IOException{
+		boolean validation = false;
+		if(gestor.getQuanJug() >= 2){
+			if(gestor.haveSelecJug()){
+					validation = true;
+			}else{
+				out.println("Debe de seleccionar 2 jugadores para jugar.");
+			}
+		}else{
+			out.println("Debe de crear almenos 2 jugadores para jugar.");
+		}
+		return validation;
+	}
+	
 	public static void escogerJuego(String tipoJuego) {
 		switch (tipoJuego) {
 		case "1":
@@ -151,6 +197,7 @@ public class Main {
 
 	public static void imprimirTablero() {
 		String[][] matriz = gestor.obtenerMatriz();
+		out.println("Jugador: " + gestor.getJugAct2());
 		for (int x = 0; x < matriz.length; x++) {
 			for (int y = 0; y < matriz[x].length; y++) {
 				
@@ -169,34 +216,58 @@ public class Main {
 			//System.out.println(" ");
 			
 		}
+		out.println("");
+		out.println("Jugador: " + gestor.getJugAct1());
 		
 	}
-	public static void moverPiezas() throws IOException{
+	public static void Jugar() throws IOException{
 		String salir = "";
+		int aux = 1;
 		
-	
-		while(salir != "E"){
-			imprimirTablero();
-			System.out.println("Ingrese la posicion actual de la pieza y la coordenada a la cual quiere mover la pieza: ");
-			String posicion = readInput();
-			String[] posiciones = posicion.split(","); 
-			String letraTablero = posiciones[1].substring(0,1).toUpperCase();
-			int posActY = verificarLetra(letraTablero);
-			String prueba;
-			char posicionActual = posiciones[0].charAt(1);
-			prueba =  String.valueOf(posicionActual);
-			int posActX = Integer.valueOf(prueba);
-			String letraTableroAMover = posiciones[1].substring(0,1).toUpperCase();
-			int posMovY = verificarLetra(letraTableroAMover);
-			char posicionAMover = posiciones[1].charAt(1);
-			prueba = String.valueOf(posicionAMover);
-			int posAMovX = Integer.valueOf(prueba);
-			out.println(posActX+"-"+posActY+"-"+posAMovX+"-"+posMovY);
-			gestor.moverPiezas(posActX,posActY,posAMovX,posMovY);
-			
+		if(validateGame() && gestor.checkTipJuego()){
+			while(salir != "E"){
+				imprimirTablero();
+				System.out.println("");
+				System.out.println("Turno de: " + getJugTurno(aux));
+				System.out.println("Ingrese la posicion actual de la pieza y la coordenada a la cual quiere mover la pieza: ");
+				String posicion = readInput();
+				if(!posicion.toUpperCase().equals("E")){
+					moverPiezas(posicion);
+				}else{
+					salir = "E";
+					out.println("Saliendo de la partida...");
+				}
+				aux += 1;
+			}
+		}else{
+			out.println("Debe seleccionar el tipo de juego para jugar");
 		}
-	
 	}
+	
+	public static void moverPiezas(String posicion) throws IOException{
+		String[] posiciones = posicion.split(","); 
+		String letraTablero = posiciones[1].substring(0,1).toUpperCase();
+		int posActY = verificarLetra(letraTablero);
+		String prueba;
+		char posicionActual = posiciones[0].charAt(1);
+		prueba =  String.valueOf(posicionActual);
+		int posActX = Integer.valueOf(prueba);
+		String letraTableroAMover = posiciones[1].substring(0,1).toUpperCase();
+		int posMovY = verificarLetra(letraTableroAMover);
+		char posicionAMover = posiciones[1].charAt(1);
+		prueba = String.valueOf(posicionAMover);
+		int posAMovX = Integer.valueOf(prueba);
+		out.println(posActX+"-"+posActY+"-"+posAMovX+"-"+posMovY);
+		gestor.moverPiezas(posActX,posActY,posAMovX,posMovY);
+	}
+	public static String getJugTurno(int aux){
+		if((aux & 1) == 0){
+			return gestor.getJugAct2();
+		}else{
+			return gestor.getJugAct1();
+		}
+	}
+	
 	public static int  verificarLetra(String letraTablero){
 		
 		switch (letraTablero) {
